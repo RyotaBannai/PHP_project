92d2c2a4d2a38f6709c181b9caefd15c4d57d2df
RyotaBannai <ryotala0528@gmail.com>
2020-02-06 17:08:45 +0900

    turn off encrypt cookie with EncryptCokkies.php, get cookie with request object's cokkie method.

--- laravel/app/Http/Controllers/StateController.php
+++ laravel/app/Http/Controllers/StateController.php
@@ -8,7 +8,10 @@ class StateController extends Controller
 {
     public function index(){
         return response()
-        ->view('state.index')
+        ->view('state.index', [
+            //cookieを取得する場合は, requestオブジェクトのcookieプロパティにアクセス.
+            'App_title' => request()->cookie('app_title'), 
+        ])
         ->cookie('app_title', 'laravel', 60 * 24 * 30); //one month
         // name, value, valid duration( minutes), path, domain, allow only via https (true or false)
     }
--- laravel/resources/views/state/index.blade.php
+++ laravel/resources/views/state/index.blade.php
@@ -2,4 +2,5 @@
 @section('title','State Management.')
 @section('main')
  <div>State management.</div>
+ <p>registered cokkie is: {{ $App_title }}</p>
 @endsection
