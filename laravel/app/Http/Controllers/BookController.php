2ff4aa42c20b7932c37db87a07d8bce691828605
RyotaBannai <ryotala0528@gmail.com>
2020-02-07 19:30:03 +0900

    Tiny change in comments.

--- laravel/app/Http/Controllers/BookController.php
+++ laravel/app/Http/Controllers/BookController.php
@@ -11,7 +11,7 @@ class BookController extends Controller
         return view('book.list', [
             //'books' => Book::where('publisher','翔泳社')->get(),
             //'books' => Book::where('price','<', 3000)->get(), //大小比較
-            'books' => Book::where('title','LIKE', '%PHP%')->get(), //部分一致
+            'books' => Book::where('title','LIKE', '%PHP%')->get(), //部分一致検索
         ]);
     }
 }
