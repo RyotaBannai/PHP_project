e8e604bd81a0fc183f58442103a3425118f9a230
RyotaBannai <ryotala0528@gmail.com>
2020-04-01 22:31:30 +0900

    namespace. global space. absolute path, relative path...

---
+++ vanilla/classes/cake.php
@@ -0,0 +1,6 @@
+<?php
+
+namespace Food\Sweets;
+class Cake{
+
+}
---
+++ vanilla/classes/name_space.php
@@ -0,0 +1,7 @@
+<?php
+namespace Other;
+require_once 'cake.php';
+// namespaceを指定したときは「完全修飾名」で指定
+// namespaceを指定しいない＝グローバルな空間
+$mycake = new \Food\Sweets\Cake();
+
