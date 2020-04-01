2c6106b22e05b524bcb5676727cfa9cde0d682d1
RyotaBannai <ryotala0528@gmail.com>
2020-04-01 23:04:07 +0900

    learn many ways to use namespace things...

--- vanilla/classes/name_space.php
+++ vanilla/classes/name_space.php
@@ -1,7 +1,13 @@
 <?php
 namespace Other;
 require_once 'cake.php';
+//use Food\Sweets; // use を使った場合、最後の被修飾名でインポートすることと等価.
+use \Food\Sweets\Cake;
+
+// use \Food\Sweets as Foods;
+// $mycake = new Foods\Cake();
+
 // namespaceを指定したときは「完全修飾名」で指定
-// namespaceを指定しいない＝グローバルな空間
-$mycake = new \Food\Sweets\Cake();
+// namespaceを指定しない＝グローバルな空間
+$mycake = new Cake();
 
