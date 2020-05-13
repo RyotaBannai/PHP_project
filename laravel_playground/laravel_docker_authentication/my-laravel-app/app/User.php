b7e09e539cdc0385bcd06f07e4693b34cb1ad3bb
RyotaBannai <ryotala0528@gmail.com>
2020-05-13 23:46:56 +0900

    sanctum backend configurations.

--- laravel_playground/laravel_docker_authentication/my-laravel-app/app/User.php
+++ laravel_playground/laravel_docker_authentication/my-laravel-app/app/User.php
@@ -5,15 +5,14 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use HasApiTokens, Notifiable;
 
     /**
      * The attributes that are mass assignable.
-     *
-     * @var array
      */
     protected $fillable = [
         'name', 'email', 'password',
@@ -21,8 +20,6 @@ class User extends Authenticatable
 
     /**
      * The attributes that should be hidden for arrays.
-     *
-     * @var array
      */
     protected $hidden = [
         'password', 'remember_token',
@@ -30,10 +27,10 @@ class User extends Authenticatable
 
     /**
      * The attributes that should be cast to native types.
-     *
-     * @var array
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
 }
